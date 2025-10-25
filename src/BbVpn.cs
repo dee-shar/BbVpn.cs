7bcb4454b3fad1ceeb9dcf775508941a083bface
huok <77536370+dee-shar@users.noreply.github.com>
2025-10-25 20:12:27 +0300

    Update and rename bb_vpn.sh to BbVpn.cs

---
+++ src/BbVpn.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace BbVpnApi
+{
+    public class BbVpn
+    {
+        private readonly HttpClient httpClient;
+        private readonly string apiUrl = "https://bbvpn.avdev.site";
+        public BbVpn()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Dalvik/2.1.0 (Linux; U; Android 9; RMX3551 Build/PQ3A.190705.003)");
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<string> getServers(string token)
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/api.php?token={token}");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> getApiParameters(string token)
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/apiParams.php?token={token}&v=v38");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
